70f76917620d0df5c040de6302ea0fc34f9994e7
Anushree-naik <anushree317@gmail.com>
2018-01-27 06:44:24 -0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution 
+{
+	public static void main(String[] args) 
+  {
+        System.out.println("Hello, World."); // String Literal "Hello,World."
+        Scanner message = new Scanner(System.in); 
+        String m = message.nextLine(); //Content of the inputString variable
+        System.out.println(m); 
+   }
+}
