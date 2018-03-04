59e14cbf8f31c163f1f840fa6e2ec6a11448efd9
Anushree-naik <anushree317@gmail.com>
2018-03-03 19:32:05 -0800

    Update helloWorld.c

--- helloWorld.c
+++ helloWorld.c
@@ -1,21 +1,20 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
 int main() {
     // Declare a variable named 'input_string' to hold our input.
-    string input_string; 
+    char input_string[105]; 
     
-    // Read a full line of input from stdin (cin) and save it to our variable, input_string.
-    getline(cin, input_string); 
+    // Read a full line of input from stdin and save it to our variable, input_string.
+    scanf("%[^\n]", input_string); 
     
-    // Print a string literal saying "Hello, World." to stdout using cout.
-    cout << "Hello, World." << endl;
-
+    // Print a string literal saying "Hello, World." to stdout using printf.
+    printf("Hello, World.\n");
+    
+    printf("%s", input_string);
     // TODO: Write a line of code here that prints the contents of input_string to stdout.
-
+    
     return 0;
 }
