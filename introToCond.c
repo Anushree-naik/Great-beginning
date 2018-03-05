4bc6697aaae263bea2bcb8dcd411898ab394f1da
Anushree-naik <anushree317@gmail.com>
2018-03-04 18:27:34 -0800

    Update introToCond.c

--- introToCond.c
+++ introToCond.c
@@ -11,11 +11,11 @@ int main()
     int n; 
     scanf("%d",&n);
     
-    if(n%2==1 || (n%2==0 && n<=6 && n<=20 ))
+    if(n%2==1 || (n%2==0 && n>=6 && n<=20 ))
     {
         printf("Weird");
     }
-    else if((n%2==1 && n<=2 && n<=5) || (n%2==0 && n>20))
+    else if((n%2==1 && n>=2 && n<=5) || (n%2==0 && n>20))
     {
         printf("Not Weird");   
     }
