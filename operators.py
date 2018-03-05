3dd8cb98ac9f74b005851ea860135a6d4c4f4f4b
Anushree-naik <anushree317@gmail.com>
2018-03-04 17:18:55 -0800

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+mealCost = float(input())
+tipPercent = int(input())
+taxPercent = int(input())
+
+tip = mealCost * tipPercent / 100
+tax = mealCost * taxPercent / 100
+total = mealCost + tip + tax
+
+print("The total meal cost is " + str(round(total)) + " dollars.")
